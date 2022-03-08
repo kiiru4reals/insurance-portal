2af751d83bdafbf4a686483177009ab8e33f94a1
kiiru4reals <smaina991@gmail.com>
2022-03-08 21:26:44 +0300

    UI improvements

--- lib/routes/vehicle_prospective.dart
+++ lib/routes/vehicle_prospective.dart
@@ -46,20 +46,10 @@ class _VehicleProspectiveState extends State<VehicleProspective> {
             ),
           ],
         ),
-        body:  
-        ScrollableWidget(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: buildDataTable(),
-                ),
-              ],
-            ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ScrollableWidget(child: buildDataTable()),
           ),
-        ),
       );
 
   Widget buildDataTable() {
