2bba597c2744ec24f3a06212c5fafe88e290583f
kiiru4reals <smaina991@gmail.com>
2022-03-15 13:14:49 +0300

    initial commit

---
+++ lib/routes/underwriters/life_underwriters.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:insurance_portal/constants/color.dart';
+import 'package:insurance_portal/providers/life_insurer_provider.dart';
+import 'package:insurance_portal/widgets/no_underwriters.dart';
+import 'package:insurance_portal/widgets/show_life_insurers.dart';
+import 'package:provider/provider.dart';
+
+class LifeUnderwriters extends StatefulWidget {
+  static const routeName = "/LifeUnderwriters";
+
+  @override
+  _LifeUnderwritersState createState() => _LifeUnderwritersState();
+}
+
+class _LifeUnderwritersState extends State<LifeUnderwriters> {
+  @override
+  Widget build(BuildContext context) {
+    final luAttributes = Provider.of<LifeInsurersProvider>(context);
+    luAttributes.fetchInsurer();
+    return luAttributes.getlifeinsurer.isEmpty
+        ? Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: ColorConsts.bgColor,
+        title: Text("No underwriters"),
+      ),
+      body: NoUnderWriters(),
+    )
+        : Scaffold(
+      appBar: AppBar(
+        title: Text(
+            'Total life insurance packages: (${luAttributes.getlifeinsurer
+                .length})'),
+      ),
+      body: ListView.builder(
+          itemCount: luAttributes.getlifeinsurer.length,
+          itemBuilder: (BuildContext ctx, int index) {
+            return ChangeNotifierProvider.value(
+              value: luAttributes.getlifeinsurer[index],
+              child: ShowLifeInsurers(
+                lifeinsurerId:
+                luAttributes.getlifeinsurer.toString()[index],
+              ),
+            );
+          }),
+    );
+  }
+}
